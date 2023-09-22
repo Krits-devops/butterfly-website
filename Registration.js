f7685f5614d84ec004bc56c6ac63fc7f46cb10e7
Krits-devops <kritagyarm9@gmail.com>
2023-09-21 20:51:19 -0400

    Registration JS update

---
+++ Registration.js
@@ -0,0 +1,63 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+$( function() {
+  var state = true;
+  $( "#button" ).on( "click", function() {
+    if ( state ) {
+      $( "#effect" ).animate({
+        backgroundColor: "#aa0000",
+        color: "#fff",
+        width: 1700
+      }, 1000 );
+    } else {
+      $( "#effect" ).animate({
+        backgroundColor: "#fff",
+        color: "#000",
+        width: 240
+      }, 1000 );
+    }
+    state = !state;
+  });
+} );
+
+// Function to handle form submission
+function submitForm(event) {
+  event.preventDefault();
+  const formData = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    date: document.getElementById('date').value,
+    time: document.getElementById('time').value,
+  };
+  // Store the form data in localStorage
+  localStorage.setItem('bookingFormData', JSON.stringify(formData));
+  // Redirect to the booking_confirmation.html page
+  window.location.href = 'booking_confirmation.html';
+}
